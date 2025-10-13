d8eb66824deaed494c8bbf1a35998100762e0c9d
boyanaaaa <125132910+boyanaaaa@users.noreply.github.com>
2025-10-13 22:25:26 +0300

    A function has been written that loops through the array and displays each book on the page.

--- program.js
+++ program.js
@@ -1,4 +1,9 @@
-function Book(title, author, pagesNumber,hasRead ){
+
+const myLibrary = [];
+
+
+function Book(title, author, pagesNumber,hasRead){
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pagesNumber = pagesNumber;
@@ -12,6 +17,36 @@ function Book(title, author, pagesNumber,hasRead ){
 
 }
 
-const theHobbit = new Book("The hobbit", "J.R.R", 295, false);
-console.log(theHobbit.info());
+function addBookToLibrary(title, author, pagesNumber,hasRead) {
+  // take params, create a book then store it in the array
+    const newBook = new Book(title, author, pagesNumber,hasRead);
+   myLibrary.push(newBook);
+}
 
+
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("Harry Potter", "J.K. Rowling", 500, true);
+
+console.log(myLibrary);
+console.log(myLibrary[0].info());
+
+
+const mainContainer = document.querySelector(".mainContainer");
+
+function displayBooks(){
+    for ( let i = 0; i < myLibrary.length; i++){
+        const book = myLibrary[i];
+        
+
+        const divBook = document.createElement("div");
+        const bookTitle = document.createElement("h3");
+        bookTitle.innerText = book.title;
+        divBook.appendChild(bookTitle);
+        mainContainer.appendChild(divBook);
+
+
+    }
+}
+
+displayBooks();
