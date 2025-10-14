a149af612dc1e961192c80c8436bf6462469958b
boyanaaaa <125132910+boyanaaaa@users.noreply.github.com>
2025-10-14 18:30:24 +0300

    It is written a function that loops through the array and displays each book on the page. The sidebar is visually and functionally completed.

--- program.js
+++ program.js
@@ -23,30 +23,70 @@ function addBookToLibrary(title, author, pagesNumber,hasRead) {
    myLibrary.push(newBook);
 }
 
-
+const mainContainer = document.querySelector(".mainContainer");
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
 addBookToLibrary("Harry Potter", "J.K. Rowling", 500, true);
+displayBooks();
 
 console.log(myLibrary);
 console.log(myLibrary[0].info());
 
 
-const mainContainer = document.querySelector(".mainContainer");
+
 
 function displayBooks(){
+    mainContainer.innerHTML = "";
+
     for ( let i = 0; i < myLibrary.length; i++){
         const book = myLibrary[i];
-        
 
         const divBook = document.createElement("div");
-        const bookTitle = document.createElement("h3");
+
+        const bookTitle = document.createElement("h2");
         bookTitle.innerText = book.title;
         divBook.appendChild(bookTitle);
         mainContainer.appendChild(divBook);
+
+        const bookAuthor = document.createElement("h3");
+        bookAuthor.innerText = book.author;
+        divBook.appendChild(bookAuthor);
+
+        const bookPages = document.createElement("p");
+        bookPages.classList.add("bookPagesp");
+        bookPages.innerText = book.pagesNumber + " pages";
+        divBook.appendChild(bookPages);
+
+        const readStatus = document.createElement("p");
+        readStatus.classList.add("readStatusp");
+        readStatus.innerText = book.hasRead ? "Read already" : "Not read yet";
+        divBook.appendChild(readStatus);
 
 
     }
 }
 
-displayBooks();
+
+
+
+const submitBtn = document.querySelector(".submitBtn");
+submitBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    const titleInput = document.querySelector(".titleiInput");
+    const title = titleInput.value;
+    const authorInput = document.querySelector(".authorInput");
+    const author= authorInput.value;
+    const pagesInput = document.querySelector(".pagesInput");
+    const pages = pagesInput.value;
+    const hasReadInput = document.querySelector(".hasReadInput");
+    const hasRead = hasReadInput.checked;
+
+    addBookToLibrary(title,author,pages,hasRead);
+    displayBooks();
+
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    hasReadInput.checked = false;
+})
